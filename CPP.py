3dc3fb4ba5411c7c3629105023b8871784ac6b8c
henry-zhang-bo-han <henryzhangbh@gmail.com>
2024-07-30 13:42:47 -0700

    Fixed payment calculation logic

--- CPP.py
+++ CPP.py
@@ -58,6 +58,15 @@ FIELDS_LIST = [
 PAGES = set([field['page'] for field in FIELDS_LIST])
 FIELD_OPTIONS = {field['ID']: field['options'] for field in FIELDS_LIST if 'options' in field}
 
+CALCULATION_FIELDS = [
+    {'ID': 'Baseline Monthly Payment (Starting at Age 65)', 'type': 'float'},
+    {'ID': 'Corrected Start Date', 'type': 'str', 'format': 'YYYY-MM-DD'},
+    {'ID': 'Delta Between Corrected Start Date and Date Turning 65 in Months', 'type': 'int'},
+    {'ID': 'Percentage Impact on Monthly Payment', 'type': 'float'},
+    {'ID': 'Adjusted Monthly Payment', 'type': 'float'},
+    {'ID': 'Rationale', 'type': 'str'}
+]
+
 
 def convert_pdf_to_images(pdf):
     images = convert_from_bytes(pdf, fmt='png', thread_count=8)
@@ -151,6 +160,83 @@ def determine_age_eligibility(date_of_birth, client):
     return completion.choices[0].message.content
 
 
+def calculate_payment(date_of_birth, pension_start, start_date, contribution_amount, client):
+    # Determine start date of payment
+    date_of_birth = datetime.strptime(date_of_birth, '%Y-%m-%d')
+    date_turning_60 = date_of_birth.replace(year=date_of_birth.year + 60)
+    date_turning_65 = date_of_birth.replace(year=date_of_birth.year + 65)
+    date_turning_70 = date_of_birth.replace(year=date_of_birth.year + 70)
+
+    if pension_start == 'As soon as I qualify':
+        pension_start_date = max(date_turning_60, datetime.today())
+    else:
+        pension_start_date = datetime.strptime(start_date, '%Y-%m')
+        pension_start_date = min(pension_start_date, date_turning_70)
+        pension_start_date = max(pension_start_date, date_turning_60)
+
+    # Calculate the difference with 65th birthday
+    delta_months = ((pension_start_date.year - date_turning_65.year) * 12 +
+                    pension_start_date.month - date_turning_65.month)
+
+    # Calculate the percentage difference in payment
+    percentage_impact = 0.0
+    if delta_months < 0:
+        percentage_impact = max(0.6 * delta_months, -36)
+    elif delta_months > 0:
+        percentage_impact = min(0.7 * delta_months, 42)
+
+    # Calculate the adjusted monthly payment
+    payment = contribution_amount / 240 * (1 + percentage_impact / 100)
+
+    # Construct the user prompt
+    user_prompt = f'''
+    The applicant was born on {date_of_birth} (YYYY-MM-DD).
+    The applicant intends to start receiving CPP pension benefits on {start_date} (YYYY-MM).
+    The applicant should start receiving benefits between the age of 60 and 70.
+    The adjusted pension start date should be {pension_start_date.strftime('%Y-%m-%d')}.
+    The applicant has made a total of ${contribution_amount:,.2f} in CPP contributions.
+    The total contributions should be spread across 20 years if the applicant starts receiving benefits at age of 65.
+    If the pension starts before age 65, it will be reduced by 0.6% for each month (or 7.2% per year) before the 65th
+    birthday. The maximum reduction is 36% if the applicant starts their pension at age 60. This reduction is permanent.
+    If the pension starts after age 65, it will increase by 0.7% for each month (or 8.4% per year) after the 65th
+    birthday. The maximum increase is 42% if starting the pension at age 70. The increase is permanent, and there
+    are no more increases after age 70.
+    The percentage delta between the projected monthly payment and the baseline monthly payment is {percentage_impact}%.
+    As such, the adjusted monthly payment is ${payment:,.2f}.
+    ---
+    Write a clear paragraph explaining the rationale behind the calculation. Retain key figures.
+    Do not have more than 2 decimal places for any number.
+    '''
+
+    # Use OpenAI to make calculations
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[{'role': 'user', 'content': user_prompt}]
+    )
+
+    return {
+        'pension_start_date': pension_start_date,
+        'delta_months': delta_months,
+        'percentage_impact': percentage_impact,
+        'payment': payment,
+        'rationale': completion.choices[0].message.content
+
+    }
+
+
+def toggle_inputs():
+    if st.session_state['toggle_confirm_accuracy'] is False:
+        st.session_state.pop('age_eligibility_assessment', None)
+        st.session_state.pop('past_contributions_assessment', None)
+        st.session_state.pop('cpp_eligible', None)
+        st.session_state.pop('payment_calculation', None)
+
+
+def toggle_eligibility():
+    if st.session_state['toggle_confirm_eligibility'] is False:
+        st.session_state.pop('payment_calculation', None)
+
+
 if __name__ == '__main__':
     # Page title
     st.title('CPP Application Processing')
@@ -170,6 +256,7 @@ if __name__ == '__main__':
 
     # Display uploaded PDF
     if 'has_uploaded_pdf' in st.session_state:
+        st.divider()
         st.subheader('Validate AI extracted inputs')
         for idx in PAGES:
             if 'toggle_confirm_accuracy' not in st.session_state:
@@ -184,10 +271,15 @@ if __name__ == '__main__':
                     else:
                         st.text_input(k, value=v, key=convert_id_to_key(k))
 
-        st.toggle('I confirm the accuracy of extracted inputs', key='toggle_confirm_accuracy')
+        st.toggle(
+            'I confirm the accuracy of extracted inputs',
+            key='toggle_confirm_accuracy',
+            on_change=toggle_inputs
+        )
 
     # Display eligibility assessment
     if 'toggle_confirm_accuracy' in st.session_state and st.session_state['toggle_confirm_accuracy']:
+        st.divider()
         st.subheader('Confirm AI assessed eligibility')
 
         # Assess age eligibility
@@ -211,7 +303,7 @@ if __name__ == '__main__':
         # Assess past contributions
         if 'past_contributions_assessment' not in st.session_state:
             with st.spinner('Assessing past contributions ...'):
-                time.sleep(1)
+                time.sleep(2)
                 if 'CONTRIBUTIONS' in st.secrets:
                     st.session_state['past_contributions_assessment'] = float(st.secrets['CONTRIBUTIONS'])
                 else:
@@ -236,3 +328,64 @@ if __name__ == '__main__':
             st.write(f'✅ {first_name} is eligible for CPP benefits')
         else:
             st.write(f'❌ {first_name} is not eligible for CPP benefits')
+
+        st.toggle(
+            'I confirm the correctness of eligibility assessments',
+            key='toggle_confirm_eligibility',
+            on_change=toggle_eligibility
+        )
+
+    # Display payment calculation
+    if 'toggle_confirm_eligibility' in st.session_state and st.session_state['toggle_confirm_eligibility']:
+        if 'cpp_eligible' in st.session_state and st.session_state['cpp_eligible']:
+            st.divider()
+            st.subheader('Confirm AI calculated payment')
+
+            # Calculate payment
+            if 'payment_calculation' not in st.session_state:
+                with st.spinner('Calculating payment ...'):
+                    st.session_state['payment_calculation'] = calculate_payment(
+                        st.session_state['input_date_of_birth'],
+                        st.session_state['input_pension_start'],
+                        st.session_state['input_as_of_date'],
+                        st.session_state['past_contributions_assessment'],
+                        openai_client
+                    )
+
+            col1, col2, col3 = st.columns(3)
+
+            with col1:
+                with st.container(border=True):
+                    st.metric('Past contributions', f'${st.session_state["past_contributions_assessment"]:,.2f}',
+                              'in total', delta_color='off')
+
+            with col2:
+                with st.container(border=True):
+                    st.metric(
+                        'Pension start date',
+                        st.session_state['payment_calculation']['pension_start_date'].strftime('%Y-%m-%d'),
+                        f"{st.session_state['payment_calculation']['delta_months']} mo from 65th birthday"
+                    )
+
+            with col3:
+                with st.container(border=True):
+                    st.metric('Adjusted monthly payment',
+                              f'${st.session_state["payment_calculation"]["payment"]:,.2f}',
+                              f'{st.session_state["payment_calculation"]["percentage_impact"]:.2f}% vs. baseline')
+
+            with st.expander(
+                    f'Detailed rationale behind ${st.session_state["payment_calculation"]["payment"]:,.2f} calculation',
+                    expanded=True):
+                st.write(st.session_state['payment_calculation']['rationale'].replace('$', '\\$'))
+
+            st.toggle('I confirm the correctness of payment calculation', key='toggle_confirm_payment')
+
+    # Display communication button
+    if 'toggle_confirm_eligibility' in st.session_state and st.session_state['toggle_confirm_eligibility']:
+        if 'cpp_eligible' in st.session_state and st.session_state['cpp_eligible']:
+            if 'toggle_confirm_payment' in st.session_state and st.session_state['toggle_confirm_payment']:
+                st.divider()
+                st.button('Approve CPP application', on_click=st.balloons)
+        elif 'cpp_eligible' in st.session_state and not st.session_state['cpp_eligible']:
+            st.divider()
+            st.button('Reject CPP application', on_click=st.snow)
